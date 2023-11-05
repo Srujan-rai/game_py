23e55531c3f259c5aaa08d96347ceed03d5f2b77
Srujan-rai <raisrujan@gmail.com>
2023-11-05 12:05:36 +0530

    code added

---
+++ guess_game.py
@@ -0,0 +1,4 @@
+import random
+
+def predict(num):
+    random_number=
---
+++ madlibs.py
@@ -0,0 +1,7 @@
+country=input("enter the country: ")
+count=input("enter the count: ")
+number=input("enter the number: ")
+
+sentence=f"hey {country} is very good country.also we have {count} population here. here {number} religin leaves"
+print(sentence)
+
